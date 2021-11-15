682b70361272fc5bdd77fa2270f2b50e9c23836d
Lir92 <lir-92@mail.ru>
2021-11-15 21:47:29 +0300

    Square root of a number

--- src/main/java/lesson4/HomeWorkLesson4.java
+++ src/main/java/lesson4/HomeWorkLesson4.java
@@ -7,6 +7,10 @@ public class HomeWorkLesson4 {
 
         Scanner number = new Scanner(System.in);
         System.out.print("Введите число для вычисления квадратного корня: ");
+        while (!number.hasNextInt()){
+            String text = number.next();
+            System.out.print( text+ " не является числом. Введите число для вычисления квадратного корня: ");
+        }
         int request = number.nextInt();
         int startNumber = 2;
 
@@ -14,6 +18,8 @@ public class HomeWorkLesson4 {
             System.out.println("Корень числа 0 = 0");
         } else if (request == 1){
             System.out.println("Корень числа 1 = 1");
+        } else if (request < 0){
+            System.out.println("Отрицательные числа не имеют квадратных корней");
         } else if (request > startNumber * startNumber){
             do {
                 if (request == startNumber * startNumber){
