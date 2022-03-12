aed8e9a8c87cb99e52fa2924d1a7053c93725f13
Lir92 <lir-92@mail.ru>
2022-03-13 01:23:39 +0500

    lecture 28 - SpringBoot start

---
+++ src/main/java/lesson28/service/impl/FoodServiceImpl.java
@@ -0,0 +1,13 @@
+package lesson28.service.impl;
+
+import lesson28.service.FoodService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FoodServiceImpl implements FoodService {
+
+    @Override
+    public void feed() {
+        System.out.println("Здесь вас накормят");
+    }
+}
